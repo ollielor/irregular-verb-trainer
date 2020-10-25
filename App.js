813f908b944be61d9f79e68224068c2853397e3f
Olli Eloranta <olli@ollieloranta.fi>
2020-10-25 19:28:24 +0200

    First skeleton of main screen

--- App.js
+++ App.js
@@ -1,21 +1,104 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { 
+   StyleProvider, 
+   Container, 
+   Button, 
+   Text, 
+   Spinner, 
+   Header,
+   Title, 
+   Content,
+   Footer,
+   FooterTab
+} from 'native-base';
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import commonColor from './native-base-theme/variables/commonColor';
+import variables from './native-base-theme/variables/variables';
+import getTheme from './native-base-theme/components'
+
+const App = () => {
+
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+      const loadFonts = async () => {
+         try {
+            await Font.loadAsync({
+            Roboto: require('native-base/Fonts/Roboto.ttf'),
+            Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+            ...Ionicons.font,
+            })
+            setIsReady(true);
+         } catch (error) {
+            console.log(error);
+         }
+      }
+      loadFonts();
+  }, [])
+
+    return (
+       <StyleProvider style={getTheme(commonColor)}>
+            <Container style={styles.container}>
+               {!isReady && 
+                  <Spinner color='#7E00C5' />
+               }
+               {isReady &&
+                  <Container>
+                        <Header>
+                           <Title>
+                              <Text style={{color: '#fff'}}>
+                                 Verbivalmentaja
+                              </Text>
+                           </Title>
+                        </Header>
+                        <Content>
+                           <Button full>
+                              <Text uppercase={false}>
+                                 Button text
+                              </Text>
+                           </Button>
+                           <Text>
+                              This is test text. This is test text. This is test text. This is test text. This is test text. This is test text. This is test text. This is test text. This is test text. This is test text. 
+                           </Text>
+                        </Content>
+                        <Footer>
+                           <FooterTab>
+                              <Text style={styles.centered}>
+                                 Koti
+                              </Text>
+                           </FooterTab>
+                           <FooterTab>
+                              <Text style={styles.centered}>
+                                 Tulokset
+                              </Text>
+                           </FooterTab>
+                           <FooterTab>
+                              <Text style={styles.centered}>
+                                 Asetukset
+                              </Text>
+                           </FooterTab>
+                        </Footer>
+                  </Container>
+               }
+            </Container>
+      </StyleProvider>
+    );
 }
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#404040',
+    //alignItems: 'center',
+    //justifyContent: 'center'
   },
-});
+  centered: {
+     color: '#fff',
+     alignContent: 'center',
+     alignItems: 'center'
+  }
+  });
