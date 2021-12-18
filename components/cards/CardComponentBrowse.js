9a830f85853e9599e0fa6c8b3970e3b054654c19
Olli Eloranta <olli@ollieloranta.fi>
2021-12-18 14:44:12 +0200

    Improvements to listing components

--- components/cards/CardComponentBrowse.js
+++ components/cards/CardComponentBrowse.js
@@ -1,29 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import { HStack, Box, Text } from 'native-base';
 import { styles } from '../../styles/styles';
+import ButtonComponentNarrow from '../buttons/ButtonComponentNarrow';
 
 const CardComponentBrowse = (props) => {
-   const [infAndPresLength, setInfAndPresLength] = useState(0);
 
-   useEffect(() => {
-      setInfAndPresLength(
-         props.verb.infinitive.length +
-         props.verb.present.length +
-         props.verb.present_alt.length
-      );
-   }, []);
+   const [addedToList, setAddedToList] = useState(true);
+
+   const addToList = () => {
+      setOwnVerbList([...ownVerbList, props.verb.meaning_id]);
+      console.log('ownVerbList after add: ', ownVerbList);
+   };
+
+   const removeFromList = (meaningId) => {
+      setOwnVerbList(ownVerbList.filter((id) => meaningId !== id));
+      console.log('ownVerbList after remove: ', ownVerbList);
+   };
 
    return (
-      <HStack style={styles(props).cardComponentGrey}>
-         <Box shadow='2' style={styles(props).browseBoxStyle}>
+      <HStack style={styles(props).cardComponentGrey} flexDirection='row'>
+         <Box shadow='2' style={styles(props).browseBoxStyle} flex='2'>
             <Text style={{ color: '#7E00C5', fontWeight: 'bold' }}>
                {props.verb.infinitive},&nbsp;
                {props.verb.present},&nbsp;
-               {infAndPresLength > 15 && `\n`}
                {props.verb.present_alt && ` / ${props.verb.present_alt}`}
-               {props.verb.past_alt &&
-                  props.verb.past_alt.length > 7 &&
-                  '\n'}
                {!props.verb.past_alt && `${props.verb.past}, `}
                {props.verb.past_alt.length > 1 &&
                   `${props.verb.past} / ${props.verb.past_alt}`}
@@ -32,6 +32,9 @@ const CardComponentBrowse = (props) => {
                   ` / ${props.verb.presperf_alt}`}
             </Text>
             <Text>{props.verb.meaning}</Text>
+         </Box>
+         <Box flex='1'>
+            <ButtonComponentNarrow title='Osaan' function={addToList} />
          </Box>
       </HStack>
    );
--- screens/BrowseScreen.js
+++ screens/BrowseScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import { ScrollView } from 'native-base';
 
 import { connect } from 'react-redux';
@@ -12,6 +12,13 @@ import Heading from '../components/styling/Heading';
 import { styles } from '../styles/styles';
 
 const BrowseScreen = (props) => {
+
+   const [ownVerbList, setOwnVerbList] = useState([]);
+   
+   const levels = [1, 2, 3];
+
+   const verbList = [];
+
    let verbs;
    if (props.language === 1) {
       verbs = props.verbsSwedish;
@@ -32,9 +39,25 @@ const BrowseScreen = (props) => {
             goBack={navigation.goBack}
          />
          <ScrollView style={styles(props).browseContainer}>
-            <Heading>Taso 1</Heading>
-            {verbs
-               .filter((verb) => verb.level === 1)
+            {levels.map((level, index) => 
+               verbs.filter((verb, idx) => verb.level === level)
+                  .sort((a, b) =>
+                     props.language === 1
+                        ? a.present > b.present
+                           ? 1
+                           : -1
+                        : a.infinitive > b.infinitive
+                           ? 1
+                           : -1
+                  ).map((verbs, idx) => verbs
+                  ).map((verbForm, idx) => (
+                     <Fragment key={index}>
+                        {idx === 0 && <Heading>Taso {level}</Heading>}
+                        <CardComponentBrowse key={level} verb={verbForm} />
+                     </Fragment>
+                  )))}
+{/*             {verbs.map((verb, index) => index < 3)
+               .filter((verb) => verb.level === index)
                .sort((a, b) =>
                   props.language === 1
                      ? a.present > b.present
@@ -45,8 +68,8 @@ const BrowseScreen = (props) => {
                         : -1
                )
                .map((verb, index) => (
-                  <CardComponentBrowse key={index} verb={verb} />
-               ))}
+               <CardComponentBrowse key={index} verb={verb} />
+            ))} */}
             <Heading>Taso 2</Heading>
             {verbs
                .filter((verb) => verb.level === 2)
